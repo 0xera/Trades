bcc061f28aa76ffd40ded15a230fbb1c30301525
0xera <gugafasa+1@gmail.com>
2020-01-15 12:44:35 +0300

    final

--- domain/src/main/java/ru/itbirds/domain/interactor/CompanyStockInteractor.java
+++ domain/src/main/java/ru/itbirds/domain/interactor/CompanyStockInteractor.java
@@ -4,13 +4,18 @@ import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import androidx.lifecycle.LiveData;
-import io.reactivex.Flowable;
+import androidx.lifecycle.MutableLiveData;
+import io.reactivex.disposables.Disposable;
 import io.reactivex.schedulers.Schedulers;
 import ru.itbirds.data.model.Company;
 import ru.itbirds.data.model.CompanyStock;
 import ru.itbirds.data.repositories.LocalRepository;
 import ru.itbirds.data.repositories.RemoteRepository;
 import ru.itbirds.domain.usecase.CompanyStockUseCase;
+
+import static ru.itbirds.data.Constants.ACTIVE_FR;
+import static ru.itbirds.data.Constants.GAINERS_FR;
+import static ru.itbirds.data.Constants.LOSERS_FR;
 
 public class CompanyStockInteractor implements CompanyStockUseCase {
     private LocalRepository mLocalRepository;
@@ -36,26 +41,50 @@ public class CompanyStockInteractor implements CompanyStockUseCase {
     }
 
     @Override
-    public Flowable<List<Company>> getMostActive() {
+    public Disposable getMostActive(MutableLiveData<Boolean> progress, MutableLiveData<Boolean> noInternet) {
         return mRemoteRepository.getMostActive().subscribeOn(Schedulers.io())
                 .repeatWhen(objectFlowable -> objectFlowable.delay(2, TimeUnit.SECONDS))
-                .observeOn(Schedulers.io());
+                .observeOn(Schedulers.io())
+                .subscribe(companies -> {
+                            insertCompanyStock(companies, ACTIVE_FR);
+                            progress.postValue(false);
+                        },
+                        throwable -> {
+                            progress.postValue(false);
+                            noInternet.postValue(true);
+                        });
     }
 
     @Override
-    public Flowable<List<Company>> getGainers() {
+    public Disposable getGainers(MutableLiveData<Boolean> progress, MutableLiveData<Boolean> noInternet) {
         return mRemoteRepository.getGainers()
                 .subscribeOn(Schedulers.io())
                 .repeatWhen(objectFlowable -> objectFlowable.delay(2, TimeUnit.SECONDS))
-                .observeOn(Schedulers.io());
+                .observeOn(Schedulers.io())
+                .subscribe(companies -> {
+                            insertCompanyStock(companies, GAINERS_FR);
+                            progress.postValue(false);
+                        },
+                        throwable -> {
+                            progress.postValue(false);
+                            noInternet.postValue(true);
+                        });
     }
 
     @Override
-    public Flowable<List<Company>> getLosers() {
+    public Disposable getLosers(MutableLiveData<Boolean> progress, MutableLiveData<Boolean> noInternet) {
         return mRemoteRepository.getLosers()
                 .subscribeOn(Schedulers.io())
                 .repeatWhen(objectFlowable -> objectFlowable.delay(2, TimeUnit.SECONDS))
-                .observeOn(Schedulers.io());
+                .observeOn(Schedulers.io())
+                .subscribe(companies -> {
+                            insertCompanyStock(companies, LOSERS_FR);
+                            progress.postValue(false);
+                        },
+                        throwable -> {
+                            progress.postValue(false);
+                            noInternet.postValue(true);
+                        });
     }
 
 
